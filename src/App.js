1027520d63ad60b51f27bb4305b98b6c84a1d9c1
ccriptdev1 <dev1@ccript.com>
2023-10-10 01:14:55 +0500

    added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Login from './pages/Login';
 import Appointments from './pages/Appointments';
+import Success from './pages/Success';
+import Cancel from './pages/Cancel';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Login></Login>}></Route>
           <Route exact path="/appointments" element={<Appointments></Appointments>}></Route>
+          <Route exact path="/success" element={<Success></Success>}></Route>
+          <Route exact path="/cancel" element={<Cancel></Cancel>}></Route>
         </Routes>
       </div>
     </BrowserRouter>
